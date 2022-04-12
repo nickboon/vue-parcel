4616cf9c7f8519b29e8b3bfcec38d86db34c482b
nickboon <boonnick@hotmail.com>
2022-04-12 01:50:28 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import PeopleList from './PeopleList.vue'
+
+export default {
+  components: {
+    PeopleList
+  }
+}
+</script>
+
+<template>
+  <PeopleList />
+</template>
---
+++ src/PeopleList.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      people:[]
+    }
+  },
+  mounted() {
+    fetch("https://services.odata.org/TripPinRESTierService/(S(j3a2sx1yypgxj1mpzjgmsrgn))/People")
+      .then(response => response.json())
+      .then(data => this.people = data.value)
+      .catch(error => console.log(error.message));
+  }
+}
+</script>
+
+<template>
+<!-- shouldn't have to wrap root component but getting eslinting from Vetur plugin -->
+<div>
+  <h1>People</h1>
+  <ul v-if="people.length">
+    <li v-for="person in people" :key="person.UserName">
+      {{ person.UserName }}
+    </li>
+  </ul>
+  <div v-else>
+    Loading...
+  </div>
+</div>
+</template>
+
+
